c2204529c431ff597417960013e6be354d492235
SpreadZhao <www.spreadzhao@outlook.com>
2023-08-14 19:45:01 +0800

    first Commit

---
+++ CustomViewTest/app/src/main/java/com/example/customviewtest/MainApplication.kt
@@ -0,0 +1,24 @@
+package com.example.customviewtest
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import android.view.Window
+
+
+class MainApplication : Application() {
+
+//    lateinit var window: TimeWindow
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+//        window = TimeWindow(this)
+//        BlockCanary.install(this, AppBlockCanaryContext()).start()
+    }
+}
